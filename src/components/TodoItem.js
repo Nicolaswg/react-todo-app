babf43cc3b29ddde74d13ca9367fd0ab042c102e
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-02-14 18:29:39 -0400

    Add css style || implement inline-styles || implement module.css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+// stylesheet
+import styles from './TodoItem.module.css';
 
 export default class TodoItem extends Component {
   render() {
-	return (
-    <li>
-      <input type="checkbox" checked={this.props.todo.completed} onChange= {() => this.props.handleChangeProps(this.props.todo.id)}/>
-	  <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+    const {completed, id, title} = this.props.todo;
+    
+    return (
+    <li className={styles.item}>
+      <input 
+	  type="checkbox"
+	  className={styles.checkbox}
+	  checked={completed}
+	  onChange= {() => this.props.handleChangeProps(id)}/>
+	  <button onClick={() => this.props.deleteTodoProps(id)}
 	  >Delete
 	  </button>
-	  {this.props.todo.title}
+    <span style={completed ? completedStyle : null}>
+      {title}
+      </span>
     </li>
 	)
   }
