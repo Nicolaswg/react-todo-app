cb5a3596c70387f9e48fc1c7d9cb65961c884fd3
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-02-14 15:05:48 -0400

    Adding basic components state and handle DOM elements events

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+  render() {
+	return (
+    <li>
+      <input type="checkbox" checked={this.props.todo.completed} onChange= {() => this.props.handleChangeProps(this.props.todo.id)}/>{this.props.todo.title}
+    </li>
+	)
+  }
+}
